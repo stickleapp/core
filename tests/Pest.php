a7b18c71d0863f30356ae329fd340bed156be58f
D Clay Smith <dclaysmith@gmail.com>
2025-03-05 00:06:17 -0100

    wip

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use StickleApp\Core\Tests\TestCase;
 
-uses(TestCase::class)->in(__DIR__);
+uses(TestCase::class)->use(RefreshDatabase::class)->in(__DIR__);
