0e06d5cb6476874c5c00c7a7d0e67c52f4d7f574
dclaysmith <226740+dclaysmith@users.noreply.github.com>
2025-04-01 23:31:10 +0000

    Fix styling

--- workbench/database/seeders/ObjectAttributesSeeder.php
+++ workbench/database/seeders/ObjectAttributesSeeder.php
@@ -12,64 +12,63 @@ use Workbench\App\Models\User;
 
 class ObjectAttributesSeeder extends Seeder
 {
+    //   "__users": {
+    //     "user_rating": {
+    //       "value": null,
+    //       "min": 1,
+    //       "max": 5,
+    //       "avg": 4.78,
+    //       "count": 23
+    //     },
+    //     "order_count": {
+    //       "value": null,
+    //       "min": 1,
+    //       "max": 8,
+    //       "avg": 1.2,
+    //       "count": 14
+    //     },
+    //     "order_item_count": {
+    //       "value": null,
+    //       "min": 1,
+    //       "max": 47,
+    //       "avg": 1.92,
+    //       "count": 14
+    //     }
+    //   },
+    //   "__groups": {
+    //     "mrr": {
+    //       "value": null,
+    //       "min": 188,
+    //       "max": 599,
+    //       "avg": 478,
+    //       "count": 3
+    //     },
+    //     "__users": {
+    //       "user_rating": {
+    //         "value": null,
+    //         "min": 1,
+    //         "max": 5,
+    //         "avg": 4.78,
+    //         "count": 23
+    //       },
+    //       "order_count": {
+    //         "value": null,
+    //         "min": 1,
+    //         "max": 12,
+    //         "avg": 1.2,
+    //         "count": 23
+    //       },
+    //       "order_item_count": {
+    //         "value": null,
+    //         "min": 1,
+    //         "max": 78,
+    //         "avg": 2.2,
+    //         "count": 23
+    //       }
+    //     }
+    //   }
+    // }'::jsonb AS model_attributes,
 
-//   "__users": {
-//     "user_rating": {
-//       "value": null,
-//       "min": 1,
-//       "max": 5,
-//       "avg": 4.78,
-//       "count": 23
-//     },
-//     "order_count": {
-//       "value": null,
-//       "min": 1,
-//       "max": 8,
-//       "avg": 1.2,
-//       "count": 14
-//     },
-//     "order_item_count": {
-//       "value": null,
-//       "min": 1,
-//       "max": 47,
-//       "avg": 1.92,
-//       "count": 14
-//     }
-//   },
-//   "__groups": {
-//     "mrr": {
-//       "value": null,
-//       "min": 188,
-//       "max": 599,
-//       "avg": 478,
-//       "count": 3
-//     },
-//     "__users": {
-//       "user_rating": {
-//         "value": null,
-//         "min": 1,
-//         "max": 5,
-//         "avg": 4.78,
-//         "count": 23
-//       },
-//       "order_count": {
-//         "value": null,
-//         "min": 1,
-//         "max": 12,
-//         "avg": 1.2,
-//         "count": 23
-//       },
-//       "order_item_count": {
-//         "value": null,
-//         "min": 1,
-//         "max": 78,
-//         "avg": 2.2,
-//         "count": 23
-//       }
-//     }
-//   }
-// }'::jsonb AS model_attributes,
-  
     /**
      * Seed the application's database.
      */
@@ -92,38 +91,38 @@ class ObjectAttributesSeeder extends Seeder
             $attributes = [];
 
             foreach ($stickleTrackedAttributes as $attribute) {
-              $attributes[$attribute] = $customer->{$attribute} ?? null;
+                $attributes[$attribute] = $customer->{$attribute} ?? null;
             }
 
             // Replace insert with upsert to handle conflicts
             DB::table("{$prefix}object_attributes")->insert([
-              'model' => 'Workbench\App\Models\Customer',
-              'object_uid' => $customer->id,
-              'model_attributes' => json_encode($attributes),
-              'synced_at' => now(),
-              'created_at' => now(),
-              'updated_at' => now(),
+                'model' => 'Workbench\App\Models\Customer',
+                'object_uid' => $customer->id,
+                'model_attributes' => json_encode($attributes),
+                'synced_at' => now(),
+                'created_at' => now(),
+                'updated_at' => now(),
             ]);
 
             $users = $customer->users;
 
             $stickleTrackedAttributes = User::$stickleTrackedAttributes ?? [];
 
-            foreach  ($users as $user) {
+            foreach ($users as $user) {
                 $attributes = [];
 
                 foreach ($stickleTrackedAttributes as $attribute) {
-                  $attributes[$attribute] = $user->{$attribute} ?? null;
+                    $attributes[$attribute] = $user->{$attribute} ?? null;
                 }
 
                 // Replace insert with upsert to handle conflicts
                 DB::table("{$prefix}object_attributes")->insert([
-                  'model' => 'Workbench\App\Models\User',
-                  'object_uid' => $user->id,
-                  'model_attributes' => json_encode($attributes),
-                  'synced_at' => now(),
-                  'created_at' => now(),
-                  'updated_at' => now(),
+                    'model' => 'Workbench\App\Models\User',
+                    'object_uid' => $user->id,
+                    'model_attributes' => json_encode($attributes),
+                    'synced_at' => now(),
+                    'created_at' => now(),
+                    'updated_at' => now(),
                 ]);
             }
         }
