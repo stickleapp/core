544724d97bc077a8174d8827ee9ccd3b5dc73d13
D Clay Smith <dclaysmith@gmail.com>
2025-07-19 22:38:06 +0000

    wip

--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -2,13 +2,14 @@
 
 namespace Workbench\Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Seed the application's database.
      */
