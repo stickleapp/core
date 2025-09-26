f3b6535d48ca370f8c803d17bc8954ea7805da48
D Clay Smith <dclaysmith@gmail.com>
2025-09-26 15:43:35 +0000

    wip

--- resources/views/components/ui/partials/responsive-tabs.blade.php
+++ resources/views/components/ui/partials/responsive-tabs.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-1 {{ $responsiveClass ?? '' }}:hidden">
+<div class="grid grid-cols-1 {{ $responsiveClass }}:hidden">
     <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
     <select id="{{ $id }}Select" aria-label="Select a tab"
         class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-2 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
--- src/Views/Components/UI/Partials/ResponsiveTabs.php
+++ src/Views/Components/UI/Partials/ResponsiveTabs.php
@@ -10,33 +10,24 @@ use Illuminate\View\View;
 
 class ResponsiveTabs extends Component
 {
-    public string $id;
 
-    public array $tabs;
-
-    public bool $hideTabs;
-
-    public string $responsiveClass;
-
-    public string $activeTab;
+    public bool $hideTabs = false;
+    public string $responsiveClass = 'md';
+    public string $activeTab = '';
 
     /**
      * Create the component instance.
+     *
+     * @return void
      */
     public function __construct(
         #[Config('stickle.routes.api.prefix')] protected ?string $apiPrefix,
-        string $id,
-        array $tabs,
-        bool $hideTabs = false,
-        string $responsiveClass = 'md',
-        string $activeTab = '',
-    ) {
-        $this->id = $id;
-        $this->tabs = $tabs;
-        $this->hideTabs = $hideTabs;
-        $this->responsiveClass = $responsiveClass;
-        $this->activeTab = $activeTab;
-    }
+        public string $id,
+        public array $tabs,
+        public bool $hideTabs = false,
+        public string $responsiveClass = 'md',
+        public string $activeTab = '',
+    ) {}
 
     public function render(): View
     {
