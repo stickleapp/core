b4f3b401f48c666fdcf6b1dc8a6473990f47ed42
D Clay Smith <dclaysmith@gmail.com>
2025-10-16 00:00:18 +0000

    wip

--- src/Actions/ExportSegmentAction.php
+++ src/Actions/ExportSegmentAction.php
@@ -30,6 +30,7 @@ class ExportSegmentAction
             throw new \Exception('Cannot get temporary file path');
         }
 
+        assert(is_string($metaData['uri']));
         $csvPath = $metaData['uri'];
 
         if (! $fd = fopen($csvPath, 'w')) {
